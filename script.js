1a8c4ffb98b631f844f702ae408fe51ce4a72797
King <francisking1st@gmail.com>
2023-09-07 01:01:17 +0100

    fixed cache

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const session = require('express-session')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5300
 
 //middleware
 app.use(morgan('dev'))
+app.use(cors());
 app.use(express.urlencoded({extended: 'false'}))
 app.use(express.json())
 app.use(session({

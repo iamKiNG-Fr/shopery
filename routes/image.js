5c9c8d968d93c30a9b0042aa807dd530206768f0
King <francisking1st@gmail.com>
2023-09-16 18:32:28 +0100

    show image upload

--- routes/image.js
+++ routes/image.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 // Multer file storage engine
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './images')
+        cb(null, './public/images')
     },
     filename: (req, file, cb)=>{
         cb(null, 'shopery_' + file.originalname)
@@ -16,12 +16,15 @@ const upload = multer({storage: fileStorageEngine})
 
 
 router.post('/single', upload.single('image'), (req, res)=>{
-    console.log(req.file);
-    res.status(200).send(`image ${req.file.filename} uploaded successful`);
+    // console.log(req.file);
+    req.flash("success", `${req.file.filename} uploaded successfully`)
+    res.status(200).redirect('/images');
+    
 })
 router.post('/multiple', upload.array('images', 10), (req, res)=>{
-    console.log(req.files);
-    res.status(200).send("multiple images uploaded successful");
+    // console.log(req.files);
+    req.flash("success", `${req.files.length} images uploaded successfully`)
+    res.status(200).redirect('/images');
 })
  
 
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const morgan = require('morgan')
 const session = require('express-session')
 const cors = require('cors')
 const app = express()
+const fs = require('fs').promises
+const path = require('path')
+const flash = require('express-flash')
 const PORT = process.env.PORT || 5300
 
 //middleware
@@ -18,8 +21,12 @@ app.use(express.json())
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 60000
+    }
 }))
+app.use(flash())
 
 //view engine
 app.set('view engine', 'ejs')
@@ -32,24 +39,40 @@ const connectDb = async () => {
     await sequelize.sync()
 }
 
+//Backend Front page
+app.get('/', (req, res)=>{
+    res.status(200).render("index");
+})
+app.get('/images', async (req, res)=>{
+    const dirpath = path.join(__dirname, './public/images')
+    const images = await fs.readdir(dirpath, (err, files)=>{
+        if(err){
+            return console.log(err);
+        }
+        return files
+    }) 
+    res.status(200).render("images", {images, message : req.flash('success')});
+})
+app.get('/docs', (req, res)=>{
+    res.status(200).render("api");
+})
+
 //routes
 const categoryRoute = require('./routes/category.js')
 const productRoute = require('./routes/product.js')
 const productTypeRoute = require('./routes/productType.js')
 const imageRoute = require('./routes/image.js')
 
-app.get('/', (req, res)=>{
-    res.status(200).render("index");
-})
-
 app.use('/image', imageRoute)
 app.use('/api/v1/category', categoryRoute)
 app.use('/api/v1/product', productRoute)
 app.use('/api/v1/productType', productTypeRoute)
+app.use((req, res, next)=>{
+    res.status(404).send("404 - The URL you visited does not exist, you dey mess up")
+})
 
 app.listen(PORT, async ()=>{
     console.log(`connected on port ${PORT}`);
     await connectDb()
     console.log('Database Connected!');
 })
-

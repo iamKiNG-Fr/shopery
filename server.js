146145d6ef814b4312c9506282dedf0441ecdffe
King <francisking1st@gmail.com>
2023-09-16 21:14:14 +0100

    debuggeddd

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', (req, res)=>{
 })
 app.get('/images', async (req, res)=>{
     // const dirpath = path.join(__dirname, './public/images')
-    const dirpath = './public/images'
+    const dirpath = '/public/images'
     // console.log('<<<<here>>>>>', dirpath);
     const images = await fs.readdir(dirpath, (err, files)=>{
         if(err){

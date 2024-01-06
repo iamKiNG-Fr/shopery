b0a77fd2acac986ebf81cabd526a38c331441f90
King <francisking1st@gmail.com>
2024-01-06 14:56:51 +0100

    multiple request bug fix 2

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -32,6 +32,7 @@ const login = (req, res)=> {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
     // Then you can send your json as response.
+    console.log(req.session,req.user);
     res.json({message:"Success", user: req.user.firstname});
 };
 

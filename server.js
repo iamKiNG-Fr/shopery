31540d7938d19fd04a7dabe2bee9e6b215197885
King-David Francis <93971653+iamKiNG-Fr@users.noreply.github.com>
2024-01-15 11:35:30 +0100

    Update server.js set samesite and secure

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use(session({
     saveUninitialized: true,
     store: myStore,
     cookie: {
-        sameSite: 'Strict',
-        maxAge: 180 * 60 * 1000
-        // secure: false
+        sameSite: 'None',
+        maxAge: 180 * 60 * 1000,
+        secure: true
     }
 }))
 app.use(passport.initialize()) // init passport on every route call.
